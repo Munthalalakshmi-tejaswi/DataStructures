d856635fc070c099ee718fdfd7d429747240d3dc
munthalalakshmitejaswi <22102A040176@mbu.asia>
2025-03-31 21:04:55 +0530

    javaDS

---
+++ StringBalancing.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class StringBalancing{
+	
+	public static void main(String []argh)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String input=sc.next();
+            boolean isBalanced = isBalancedString(input);
+            System.out.println(isBalanced);
+        }
+    }
+
+    private static boolean isBalancedString(String line) 
+    {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : line.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+	}
+		
+}
